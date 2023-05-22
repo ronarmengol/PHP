f346b4688ce9461bf362d2c35b8971d6ac879a96
Ron Armengol <ronarmengol1977@gmail.com>
2023-05-22 23:17:13 +0200

    added html boilerplate to post.php

--- lesson1/post.php
+++ lesson1/post.php
@@ -5,3 +5,18 @@ declare(strict_types=1);
 *           This is a test for git pull
 I want to see how I can use git pull in the future
 **************************************************/
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Git practise</h1>
+</body>
+</html>
