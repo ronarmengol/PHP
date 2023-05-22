570b6f67c5ef12409ed8c65f1076a8b87ae0626b
Ron Armengol <ronarmengol1977@gmail.com>
2023-05-22 22:53:32 +0200

    put a banner in post.php

---
+++ lesson1/post.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+
+/**************************************************
+*           This is a test for git pull
+I want to see how I can use git pull in the future
+**************************************************/
